47bbf5f4a8c691acbc0b0e88a8cd12e240a2144f
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-12 10:32:55 -0300

    commit del lunes

--- src/components/header/Contacts.jsx
+++ src/components/header/Contacts.jsx
@@ -1,43 +1,43 @@
 import React from 'react'
 
 const Contacts = () => {
-  return (
-    <>
-    <section className='section-contacts'>
+    return (
+        <>
+            <section className='section-contacts'>
 
-        <article className='new-icons'>
-        <div class="wrapper">
-         <div class="button">
-            <div class="icon">
-               <i class="fab fa-facebook-f"></i>
-            </div>
-            <span>Facebook</span>
-         </div>
-         <div class="button">
-            <div class="icon">
-               <i class="fab fa-google"></i>
-            </div>
-            <span>Gmail</span>
-         </div>
-         <div class="button">
-            <div class="icon">
-               <i class="fab fa-instagram"></i>
-            </div>
-            <span>Instagram</span>
-         </div>
-         <div class="button">
-            <div class="icon">
-                <i class="fab fa-whatsapp"></i>
-            </div>
-            <span>WhatsApp</span>
-         </div>
-      </div>
-        </article>
+                <article className='new-icons'>
+                    <div class="wrapper">
+                        <a target='blank' href="https://www.facebook.com/profile.php?id=61561378967321" class="button">
+                            <div class="icon">
+                                <i class="fab fa-facebook-f"></i>
+                            </div>
+                            <span>Facebook</span>
+                        </a>
+                        <a target='blank' href="mailto:facildigitalcontacto@gmail.com?subject=Consulta&body=Hola!%20Quiero%20mi%20pagina%20web!" class="button">
+                            <div class="icon">
+                                <i class="fab fa-google"></i>
+                            </div>
+                            <span>Gmail</span>
+                        </a>
+                        <a target='blank' href="https://www.instagram.com/facildigital_arg/?hl=en" class="button">
+                            <div class="icon">
+                                <i class="fab fa-instagram"></i>
+                            </div>
+                            <span>Instagram</span>
+                        </a>
+                        <a target='blank' href="https://wa.me/543794606980?text=Hola!%20Quiero%20mi%20pagina%20web!" class="button">
+                            <div class="icon">
+                                <i class="fab fa-whatsapp"></i>
+                            </div>
+                            <span>WhatsApp</span>
+                        </a>
+                    </div>
+                </article>
 
-      
-    </section>
-    </>
-  )
+
+            </section>
+        </>
+    )
 }
 
 export default Contacts
