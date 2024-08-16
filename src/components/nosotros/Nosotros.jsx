1f90493c0b8b466d05ee4d16229c433ea3b96067
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-16 17:41:09 -0300

    commit con fotos animadas

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -17,14 +17,14 @@ const Nosotros = () => {
                 <div className='caja-info-staff'>
                     <div className='info-staff-child'>
 
-                        <img src="https://placehold.co/500x600" alt="munie 1" />
+                        <img className='staff-photo' src="https://placehold.co/500x600" alt="munie 1" />
 
                         <h4>info muñeco 1</h4>
                         <h5>laburo q hace nada</h5>
                     </div>
                     <div className='info-staff-child'>
 
-                        <img src="https://placehold.co/500x600" alt="munie 2" />
+                        <img className='staff-photo' src="https://placehold.co/500x600" alt="munie 2" />
 
                         <h4>info muñeco 2</h4>
                         <h5>laburo q hace todo</h5>
