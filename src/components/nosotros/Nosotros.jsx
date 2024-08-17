07d9353a27706a9c2c7aeb838b214ca07c4a31cb
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-17 20:40:22 -0300

    commit para llevarme a la notebooko

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -5,7 +5,7 @@ const Nosotros = () => {
     return (
         <>
             {/* MOBILE */}
-            <main className='hiddenInDesktop sobreNosotros-mobile'>
+            <main className='hiddenInDesktop animate__animated animate__fadeIn sobreNosotros-mobile'>
                 <h1>Sobre Nosotros</h1>
                 <p>Conoce al equipo detrás de Fácil Digital. <strong>Somos una startup apasionada por el diseño y desarrollo web</strong>, comprometida con ofrecer soluciones innovadoras y personalizadas a cada cliente.
 
@@ -14,6 +14,7 @@ const Nosotros = () => {
                 <div className='mobile-slider'>
                     <NosotrosSlider />
                 </div>
+                <h2>Nosotros: Fácil Digital</h2>
                 <div className='caja-info-staff'>
                     <div className='info-staff-child'>
 
@@ -32,9 +33,35 @@ const Nosotros = () => {
                 </div>
             </main>
             {/* DESKTOP */}
-            <main className="hiddenInMobile">
+            
+            <main className='animate__animated animate__fadeIn hiddenInMobile sobreNosotros-desktop'>
+                <h1>Sobre Nosotros</h1>
+                <p>Conoce al equipo detrás de Fácil Digital. <strong>Somos una startup apasionada por el diseño y desarrollo web</strong>, comprometida con ofrecer soluciones innovadoras y personalizadas a cada cliente.
 
+                </p>
+                <strong>Nuestra misión es ayudarte a destacar en el entorno digital con creatividad y profesionalismo.</strong>
+                <div className='mobile-slider'>
+                    <NosotrosSlider />
+                </div>
+                <h2>Nosotros: Fácil Digital</h2>
+                <div className='caja-info-staff-desktop'>
+                    <div className='info-staff-child'>
+
+                        <img className='staff-photo-desktop' src="https://placehold.co/500x600" alt="munie 1" />
+
+                        <h4>info muñeco 1</h4>
+                        <h5>laburo q hace nada</h5>
+                    </div>
+                    <div className='info-staff-child'>
+
+                        <img className='staff-photo-desktop' src="https://placehold.co/500x600" alt="munie 2" />
+
+                        <h4>info muñeco 2</h4>
+                        <h5>laburo q hace todo</h5>
+                    </div>
+                </div>
             </main>
+            
         </>
     )
 }
